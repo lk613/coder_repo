de19a63faa5e03f3484368f715a0455410b1ea59
lxz <lxz.613@qq.com>
2025-03-02 19:11:48 +0800

    [Inverse Pairs] complete inverse pairs by merge sort algorithm

---
+++ sto/binary_search/inverse_pairs.h
@@ -0,0 +1,17 @@
+#ifndef INVERSE_PAIRS_H
+#define INVERSE_PAIRS_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ * 
+ * @param nums int整型一维数组 
+ * @param numsLen int nums数组长度
+ * @return int整型
+ */
+int InversePairs(int* nums, int numsLen );
+
+#endif
