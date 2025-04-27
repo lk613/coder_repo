09f1c28e1a67661ce8f6c6a418046e2a5625b5af
lxz <lxz.613@qq.com>
2025-04-27 22:56:59 +0800

    [LEETCODE 53] use divide and conquer complete

--- leetcode/53_最大子数组和/MaxSubArray.cpp
+++ leetcode/53_最大子数组和/MaxSubArray.cpp
@@ -26,6 +26,7 @@ int maxSubArray(std::vector<int> &nums)
 }
 #endif
 
+#if 0
 // Dynamic Programming
 int maxSubArray(std::vector<int> &nums)
 {
@@ -48,6 +49,65 @@ int maxSubArray(std::vector<int> &nums)
 
     return max_sum;
 }
+#endif
+
+static int getCrossMax(std::vector<int> &nums, int left, int right)
+{
+    int mid = left + (right - left) / 2;
+
+    int left_sum = 0;
+    int left_max = INT_MIN;
+
+    for (int i = mid; i >= left; i--)
+    {
+        left_sum += nums[i];
+        left_max = (left_sum > left_max) ? left_sum : left_max;
+    }
+
+    int right_sum = 0;
+    int right_max = INT_MIN;
+
+    for (int i = mid + 1; i <= right; i++)
+    {
+        right_sum += nums[i];
+        right_max = (right_sum > right_max) ? right_sum : right_max;
+    }
+
+    int max_sum = left_max + right_max;
+    return max_sum;
+}
+
+static int getMax(std::vector<int> &nums, int left, int right)
+{
+    if (left == right)
+    {
+        return nums[left];
+    }
+
+    int mid = left + (right - left) / 2;
+    
+    int left_max = getMax(nums, left, mid);
+    int right_max = getMax(nums, mid + 1, right);
+    int cross_max = getCrossMax(nums, left, right);
+
+    int max_sum = (left_max > right_max) ? left_max : right_max;
+    max_sum = (max_sum > cross_max) ? max_sum : cross_max;
+
+    return max_sum;
+}
+
+// Divide and Conquer
+int maxSubArray(std::vector<int> &nums)
+{
+    if (nums.size() <= 0)
+    {
+        return 0;
+    }
+
+    int max_sum = getMax(nums, 0, nums.size() - 1);
+    return max_sum;
+}
+
 
 void TestLeetcode53()
 {
@@ -55,9 +115,9 @@ void TestLeetcode53()
     int nums[9] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
     std::vector<int> array(std::begin(nums), std::end(nums));
 #else
-    std::vector<int> array = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    // std::vector<int> array = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
 
-    // std::vector<int> array = {5,4,-1,7,8};
+    std::vector<int> array = {5,4,-1,7,8};
 #endif
 
     std::cout << "输入数组为: " << std::endl;
